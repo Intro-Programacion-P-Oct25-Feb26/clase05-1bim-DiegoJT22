7f8de160b7bf6f805e873462ff0c441470e8f56c
DiegoJT22 <daigoestrlar22@gmail.com>
2025-11-13 11:20:55 -0500

    Commit 3 clase2 Semana5

---
+++ ejemplos/Ejemplo3/src/ejemplo3/Ejemplo8.java
@@ -0,0 +1,77 @@
+/*
+ Realizar un programa en java que permita determinar si un estudiante ha pasado 
+el ciclo se ingresa el promedio del estudiante
+- Un estudiante pasa el ciclo si tiene un promedio mayor o igual a 7.5 
+
+- Si el estudiante aprueba el ciclo, presentar un mensaje como sigue, 
+Estudiante aprobado con un promedio: ?
+- Si el estudiante tiene una nota mayor o igual a 5 y menor o igual a 7.4 
+presentar un mensaje con el siguiente texto; Estudiante en suspenso con un promedio: ? 
+- Si el estudiante tiene una nota menor a 5 presentar un mensaje con el 
+siguiente texto; Estudiante reprobado con un promedio: ? 
+ */
+package ejemplo3;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo8 {
+
+    public static void main(String[] args) {
+        // Condicionales aninados
+        // >= 7.5 Aprobado
+        // >= 5 and < 7.5 Suspenso
+        // >= 3 and < 5 Recuperación
+        // < 3 Reprobado
+        double promedio = 2.978;
+        String mensaje;
+        mensaje = "";
+
+        if ((promedio >= 7.5) && (promedio <= 10)) {
+            //System.out.printf("Estudiante aprobado con un "
+            //      + "promedio: %.2f\n", promedio);
+
+            //mensaje = ("Estudiante aprobado con un promedio:\n"+promedio);
+            mensaje = String.format("Estudiante aprobado con un "
+                    + "promedio:\n %.2f\n", promedio);
+        } else {
+            if ((promedio >= 5) && (promedio < 7.5)) {
+                //System.out.printf("Estudiante en suspenso con un "
+                //        + "promedio: %.2f\n", promedio);
+
+                mensaje = String.format("Estudiante aprobado con un "
+                        + "promedio:\n %.2f\n", promedio);
+            } else {
+                if ((promedio >= 3) && (promedio < 5)) {
+                    //System.out.printf("Estudiante en recuperacion con un promedio:"
+                    //        + "%.2f\n", promedio);
+                    mensaje = String.format("Estudiante en recuperacion con un "
+                            + "promedio:\n %.2f\n", promedio);
+                } else {
+                    if ((promedio >= 0) && (promedio < 3)) {
+
+                        //System.out.printf("Estudiante reprobado con un "
+                        //        + "promedio: %.2f\n", promedio);
+                        mensaje = String.format("Estudiante reprobado con un "
+                                + "promedio:\n %.2f\n", promedio);
+                    } else {
+                        if (promedio > 0) {
+                            //System.out.printf("Nota fuera de rango negativa"
+                            //        + "\n%.2f");
+                            mensaje = String.format("Nota fuera de rango:\n %.2f\n", promedio);
+                        } else {
+                            //System.out.printf("Nota fuera de rango"
+                            //      + "\n%.2f", promedio);
+                            mensaje = String.format("Nota fuera de rango\n "
+                                    + "%.2f\n", promedio);
+
+                        }
+                    }
+                }
+            }
+        }
+        System.out.printf("%s", mensaje);
+    }
+
+}
